107feead92ed6b007c46e3a2af020305e0b86064
Kyler Jensen <kjens93@users.noreply.github.com>
2020-07-17 22:53:57 -0600

    chore: updated configuration files

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const escape = require('escape-string-regexp');
-const package = require('../package.json');
+const thisPackage = require('./package.json');
+const rootPackage = require('../package.json');
 const blacklist = require('metro-config/src/defaults/blacklist');
 
 const root = path.resolve(__dirname, '..');
 
-const peerDeps = ['@expo/vector-icons', ...Object.keys(package.peerDependencies)];
+const peerDeps = Object.keys(rootPackage.peerDependencies);
+
+const extraDeps = Object.keys(thisPackage.dependencies);
 
 module.exports = {
   projectRoot: __dirname,
@@ -15,7 +18,7 @@ module.exports = {
   // So we blacklist them at the root, and alias them to the versions in example's node_modules
   resolver: {
     blacklistRE: blacklist(peerDeps.map(m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`))),
-    extraNodeModules: peerDeps.reduce((acc, name) => {
+    extraNodeModules: [...extraDeps, ...peerDeps].reduce((acc, name) => {
       acc[name] = path.join(__dirname, 'node_modules', name);
       return acc;
     }, {}),
