e42c523244a56ef3d96b9b5b7caa30a1695ec407
Andrei Alecu <andreialecu@users.noreply.github.com>
2021-06-24 19:07:04 +0300

    fix: use custom stable sort function to fix hermes

--- src/utilities/matchFontFace.ts
+++ src/utilities/matchFontFace.ts
@@ -5,18 +5,30 @@ import { FontWeight } from '../types/FontWeight';
 import { normalizeFontWeight } from './normalizeFontWeight';
 
 /**
+ * The Hermes JS engine does not do a stable sort: https://github.com/facebook/hermes/issues/212
+ */
+export function stableSort<T>(array: T[], ...compareFns: Array<(a: T, b: T) => number>) {
+  return compareFns.reduce((final, compareFn) => {
+    return final
+      .map((item, index) => ({ item, index }))
+      .sort((a, b) => compareFn(a.item, b.item) || a.index - b.index)
+      .map(({ item }) => item)
+  }, array);
+}
+
+/**
  * Uses a font matching algorithm to choose from the list of given font faces,
  * the most appropriate font face (if any) to use for the given text style.
  *
  * @see https://www.w3.org/TR/CSS2/fonts.html
  */
 export function matchFontFace(fontFaces: FontFace[], textStyle: TextStyle): FontFace | undefined {
-  return fontFaces
-    .filter(ff => filterFontFamilyincludes(ff, textStyle))
-    .sort((a, b) => compareFontWeightDistance(a, b, textStyle))
-    .sort((a, b) => compareFontStyleExactMatch(a, b, textStyle))
-    .sort((a, b) => compareFontFamilyStartsWith(a, b, textStyle))
-    .find(() => true);
+  return stableSort(
+    fontFaces.filter(ff => filterFontFamilyincludes(ff, textStyle)),
+    (a, b) => compareFontWeightDistance(a, b, textStyle),
+    (a, b) => compareFontStyleExactMatch(a, b, textStyle),
+    (a, b) => compareFontFamilyStartsWith(a, b, textStyle)
+  ).find(() => true)
 }
 
 function filterFontFamilyincludes(ff: FontFace, textStyle: TextStyle): boolean {
